99d66325da9817091608fc38e105cbd1e60b6d24
DHJheng <aw091215@gmail.com>
2023-06-16 03:21:10 +0800

    setup

---
+++ global.c
@@ -0,0 +1,11 @@
+#include "global.h"
+
+// variables for global usage
+const float FPS = 60.0;
+const int WIDTH = 400;
+const int HEIGHT = 600;
+ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+ALLEGRO_TIMER *fps = NULL;
+bool key_state[ALLEGRO_KEY_MAX] = {false};
+bool judge_next_window = false;
+int menu_button_type = 0;
---
+++ scene.h
@@ -0,0 +1,16 @@
+#include "global.h"
+#include "charater.h"
+
+void menu_init();
+void menu_process(ALLEGRO_EVENT event);
+void menu_draw();
+void menu_destroy();
+
+void game_scene_init();
+void game_scene_draw();
+void game_scene_destroy();
+
+void intro_scene_init();
+void intro_scene_process(ALLEGRO_EVENT event);
+void intro_scene_draw();
+void intro_scene_destroy();
